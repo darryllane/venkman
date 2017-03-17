cf6256e2c17c10ab03573de7dd24dea538a1e85c
Darryl Lane <darryllane101@gmail.com>
2017-03-17 11:59:52 +0000

    Pypi update[C

--- setup.py
+++ setup.py
@@ -2,17 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='Venkman',
-    version='1.0',
+    version='0.5',
     author='Darryl lane',
     author_email='DarrylLane101@gmail.com',
     url='https://github.com/darryllane/venkman',
     packages=['Venkman'],
     include_package_data=True,
     license='LICENSE.txt',
-    description='''
-    Service account and logged in user enumeration. Used to hunt for priv accounts''',
-    long_description=open('README.md').read(),
-    scripts=['/'],
+    scripts=['Venkman/venkman'],
+    long_description='Service account and Logged in user enumeration. used for hunting priv accounts',
     install_requires=[
     ],
 )

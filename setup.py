de3338af57902c6632330ca06637c54cdee141d3
Darryl Lane <darryllane101@gmail.com>
2017-05-15 21:32:10 +0100

    BugFixes

--- setup.py
+++ setup.py
@@ -2,11 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Venkman',
-<<<<<<< HEAD
-    version='1.6.6',
-=======
-    version='1.6.5',
->>>>>>> 13bf0a5fb52f6aa6a0b604618f4b4961cfe2317a
+    version='1.6.7',
     author='Darryl lane',
     author_email='DarrylLane101@gmail.com',
     url='https://github.com/darryllane/venkman',
